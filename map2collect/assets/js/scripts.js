777d9f64bbd31947278c4d7d22b0bf3362335a82
greedwasgood <56102545+greedwasgood@users.noreply.github.com>
2019-10-30 13:38:12 +0500

    11

--- map2collect/assets/js/scripts.js
+++ map2collect/assets/js/scripts.js
@@ -63,7 +63,6 @@ function init()
     wikiLanguage['en-us'] = 'https://github.com/jeanropke/RDR2CollectorsMap/wiki/RDO-Collectors-Map-User-Guide-(English)';
     wikiLanguage['fr-fr'] = 'https://github.com/jeanropke/RDR2CollectorsMap/wiki/RDO-Collectors-Map-Guide-d\'Utilisateur-(French)';
     wikiLanguage['pt-br'] = 'https://github.com/jeanropke/RDR2CollectorsMap/wiki/Guia-do-Usu%C3%A1rio---Mapa-de-Colecionador-(Portuguese)';
-	wikiLanguage['ru'] = 'https://github.com/jeanropke/RDR2CollectorsMap/wiki/RDO-%D0%A0%D1%83%D0%BA%D0%BE%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F-%D0%BF%D0%BE-%D0%BA%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D0%BE%D0%B9-%D0%BA%D0%B0%D1%80%D1%82%D0%B5-(Russian)';
 
     if(typeof $.cookie('removed-items') === 'undefined')
         $.cookie('removed-items', '', {expires: resetMarkersDaily ? 1 : 999});
