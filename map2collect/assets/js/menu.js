e7afb873b25eab3a6b3c828b1778d3f81bfac6a5
greedwasgood <56102545+greedwasgood@users.noreply.github.com>
2019-11-07 10:31:54 +0500

    123

---
+++ map2collect/assets/js/menu.js
@@ -0,0 +1,76 @@
+/**
+ * Created by Jean on 2019-10-09.
+ */
+
+var Menu = {};
+Menu.refreshMenu = function () {
+    $.each(categories, function (key, value) {
+
+        $('.menu-hidden[data-type=' + value + ']').children('p.collectible').remove();
+
+        markers.filter(function (item) {
+            if (item.day == day && item.icon == value) {
+                if (item.subdata == null) {
+                    $('.menu-hidden[data-type=' + value + ']').append('<p class="collectible" data-type="' + item.text + '">' + languageData[lang][item.text + '.name'] + '</p>');
+                }
+                else {
+                    if ($(`.menu-hidden[data-type='american-flowers']`).children(`p.collectible[data-type='${item.subdata}']`).length > 0)
+                        return;
+
+                    var tempName = languageData[lang][item.text + '.name'];
+
+
+                    $('.menu-hidden[data-type=' + value + ']').append('<p class="collectible" data-type="' + item.subdata + '">' + tempName.split('#')[0] + '</p>');
+                }
+            }
+        });
+
+        $('.menu-hidden[data-type=treasure]').children('p.collectible').remove();
+
+        treasureData.filter(function (item) {
+            $('.menu-hidden[data-type=treasure]').append('<p class="collectible" data-type="' + item.text + '">' + languageData[lang][item.text] + '</p>');
+        });
+    });
+    $.each(disableMarkers, function (key, value) {
+        if (value.length > 0) {
+            $('[data-type=' + value + ']').addClass('disabled');
+        }
+    });
+
+    $.each(plantsDisabled, function (key, value) {
+        if (value.length > 0) {
+            $('[data-type=' + value + ']').addClass('disabled');
+        }
+    });
+
+    $.each(treasureDisabled, function (key, value) {
+        if (value.length > 0) {
+            $('[data-type=' + value + ']').addClass('disabled');
+        }
+    });
+};
+
+
+Menu.showAll = function() {
+    $.each (categoryButtons, function (key, value) {
+        $(value).children('span').removeClass("disabled")
+    });
+    enabledTypes = categories;
+    Map.addMarkers();
+};
+
+Menu.hideAll = function()
+{
+    $.each (categoryButtons, function (key, value) {
+        $(value).children('span').addClass("disabled")
+    });
+
+    enabledTypes = [];
+    Map.addMarkers();
+};
+
+Menu.refreshItemsCounter = function()
+{
+    $('.collectables-counter').text(languageData[lang]['menu.collectables_counter'].replace('{count}', (disableMarkers.length)).replace('{max}', Object.keys(visibleMarkers).length));
+};
+
---
+++ map2collect/assets/js/routes.js
@@ -0,0 +1,102 @@
+/**
+ * Created by Jean on 2019-10-09.
+ */
+
+var Routes = {};
+
+Routes.loadRoutesData = function()
+{
+
+    $.getJSON('data/routes/day_1.json', {}, function (data) {
+        routesData[1] = data;
+    });
+    $.getJSON('data/routes/day_2.json', {}, function (data) {
+        routesData[2] = data;
+    });
+    $.getJSON('data/routes/day_3.json', {}, function (data) {
+        routesData[3] = data;
+    });
+};
+
+Routes.exportCustomRoute = function ()
+{
+    const el = document.createElement('textarea');
+    el.value = customRouteConnections.join(',');
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el)
+
+    alert(languageData[lang]['routes.exported']);
+};
+
+Routes.importCustomRoute = function() {
+    var input = prompt(languageData[lang]['routes.import_prompt'], "");
+
+    if (input == null || input == "")
+    {
+        alert(languageData[lang]['routes.empty']);
+    }
+    else
+    {
+        Routes.loadCustomRoute(input);
+    }
+};
+
+Routes.loadCustomRoute = function(input)
+{
+    try
+    {
+        var connections = [];
+        input = input.replace(/\r?\n|\r/g, '').replace(/\s/g, '');
+        $.each(input.split(','), function (key, value) {
+            connections.push(visibleMarkers[value]._latlng);
+        });
+
+        if (polylines instanceof L.Polyline) {
+            baseMap.removeLayer(polylines);
+        }
+
+        polylines = L.polyline(connections, {'color': '#9a3033'});
+        baseMap.addLayer(polylines);
+    }
+    catch(e)
+    {
+        alert(languageData[lang]['routes.invalid']);
+    }
+};
+
+
+Routes.addMarkerOnCustomRoute = function(value)
+{
+    if(customRouteEnabled)
+    {
+        if(customRouteConnections.includes(value))
+        {
+            customRouteConnections = customRouteConnections.filter(function(item) {
+                return item !== value
+            })
+        }
+        else
+            customRouteConnections.push(value);
+
+
+        var connections = [];
+
+        $.each(customRouteConnections, function (key, item)
+        {
+            connections.push(visibleMarkers[item]._latlng);
+        });
+
+
+        if (polylines instanceof L.Polyline)
+        {
+            baseMap.removeLayer(polylines);
+        }
+
+        polylines = L.polyline(connections, {'color': '#9a3033'});
+        baseMap.addLayer(polylines);
+
+    }
+
+};
